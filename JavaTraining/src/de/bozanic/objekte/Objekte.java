6bc6c566534b84673ee395378967a06ace894790
Boro Bozanic <bozanic42@googlemail.com>
2014-05-03 09:49:06 +0200

    Initial checkIn

---
+++ JavaTraining/src/de/bozanic/objekte/Objekte.java
@@ -0,0 +1,14 @@
+package de.bozanic.objekte;
+
+public class Objekte {
+
+	public static void main(String[] args) {
+
+		displayGreetings();
+
+	}
+	public static void displayGreetings(){
+		System.out.println("ich bin eine Methode");
+	}
+
+}
